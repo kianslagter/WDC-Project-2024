61a9bc3a183caae47820a0d71376e19650b0d240
Sean Priestley <83564573+Sean-Priestley@users.noreply.github.com>
2024-05-13 11:02:16 +0000

    Added ability to toggle visibility of filters

--- public/App.js
+++ public/App.js
@@ -8,11 +8,41 @@ const navitems = [
     { title:'Login', url:'/login'}
 ];
 
+const testEvents = [
+    {
+        title: 'Event 1 Title',
+        description: 'Description of event 1',
+        date: '1/7/24',
+        startTime: '1pm',
+        endTime: '3pm',
+        location: 'Adelaide'
+    },
+    {
+        title: 'Event 2 Title',
+        description: 'Description of event 2, which is a bit longer than event 1 so that it breaks across multiple lines',
+        date: '3/7/24',
+        startTime: '12pm',
+        endTime: '5pm',
+        location: 'Sydney'
+    },
+    {
+        title: 'Event 3 Title',
+        description: 'Description of event 3, which is a bit longer than event 1 so that it breaks across multiple lines',
+        date: '3/8/24',
+        startTime: '12pm',
+        endTime: '5pm',
+        location: 'Adelaide'
+    }
+];
+
 createApp({
 data() {
     return {
         message: 'Hello Vue!',
-        navitems: navitems
+        navitems: navitems,
+        logged_in: false,
+        events_results: testEvents,
+        show_events_filters: false
     };
 }
 }).mount('#app');
