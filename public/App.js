cde4b07673650769014703dea24191db20872ba2
Kian Slagter <kianslagter@gmail.com>
2024-05-14 09:32:47 +0000

    individual branch pages

--- public/App.js
+++ public/App.js
@@ -47,7 +47,8 @@ const testBranchSummary = [
         phone: '0412345678',
         email: 'adelaidebranch@mealmates.com',
         description: 'The Adelaide branch of Meal Mates is located in the heart of the city. This branch has been serving the community for over a decade, providing nutritious meals to those in need. With a dedicated team of volunteers, they work tirelessly to prepare and distribute food. They also collaborate with local farms and businesses to source fresh ingredients, ensuring that every meal is not only filling but also healthy.',
-        image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg'
+        image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg',
+        page_url: '/branches/adelaidebranch'
     },
     {
         id: 2,
@@ -57,7 +58,8 @@ const testBranchSummary = [
         phone: '0412345678',
         email: 'sydneybranch@mealmates.com',
         description: 'Situated in the bustling city of Sydney, the Meal Mates branch here is known for its large-scale operations. They have a vast network of volunteers who collect surplus food from restaurants, supermarkets, and households, and distribute it to people in need. This branch has made a significant impact in reducing food waste in the city while ensuring that no one goes hungry.',
-        image_url: '/not_an_image.png'
+        image_url: '/not_an_image.png',
+        page_url: '/branches/sydneybranch'
     },
     {
         id: 3,
@@ -67,7 +69,8 @@ const testBranchSummary = [
         phone: '0412345678',
         email: 'melbournebranch@mealmates.com',
         description: 'The Melbourne branch of Meal Mates is renowned for its innovative approach to tackling food insecurity. They run community kitchens where people in need are invited to share a meal, fostering a sense of community and belonging. This branch not only provides food relief but also organizes cooking classes and nutrition workshops, empowering individuals to make healthy food choices.',
-        image_url: '/not_an_image.png'
+        image_url: '/not_an_image.png',
+        page_url: '/branches/melbournebranch'
     },
 ]
 
