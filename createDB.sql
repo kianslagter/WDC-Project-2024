835f698651e4b57ed70f314a6550cef860c0d901
Sean Priestley <83564573+Sean-Priestley@users.noreply.github.com>
2024-05-29 09:00:36 +0000

    added email to users

--- createDB.sql
+++ createDB.sql
@@ -5,10 +5,11 @@ CREATE TABLE users (
     user_id BINARY(16) DEFAULT (UUID_TO_BIN(UUID())),
     username VARCHAR(64) UNIQUE,
     password_hash VARCHAR(100) NOT NULL,
-    postcode INT,
-    first_name VARCHAR(64),
-    last_name VARCHAR(64),
-    phone_num VARCHAR(12),
+    postcode INT, -- Add check for validity (4 digits, not negative)
+    first_name VARCHAR(64) NOT NULL,
+    last_name VARCHAR(64) NOT NULL,
+    phone_num VARCHAR(14), -- Add check for validity
+    email VARCHAR(320) NOT NULL, -- Add check for validity
     system_admin BOOLEAN DEFAULT FALSE,
 
     PRIMARY KEY (user_id)
@@ -22,21 +23,21 @@ CREATE TABLE branches (
     city VARCHAR(64),
     branch_state VARCHAR(64),
     postcode INT,
-    email VARCHAR(64),
+    email VARCHAR(64) NOT NULL,
     phone VARCHAR(12),
     image_url VARCHAR(256),
-    branch_description VARCHAR(1024),
+    branch_description VARCHAR(1024) NOT NULL,
 
     PRIMARY KEY (branch_id)
 );
 
 CREATE TABLE events (
     event_id INT AUTO_INCREMENT,
-    branch_id INT,
-    event_name VARCHAR(64),
-    start_date_time DATETIME,
-    end_date_time DATETIME,
-    event_description VARCHAR(2048),
+    branch_id INT NOT NULL,
+    event_name VARCHAR(64) NOT NULL,
+    start_date_time DATETIME NOT NULL,
+    end_date_time DATETIME, -- Check after start time?
+    event_description VARCHAR(2048) NOT NULL,
     is_public BOOLEAN DEFAULT FALSE,
 
     PRIMARY KEY (event_id),
@@ -45,9 +46,9 @@ CREATE TABLE events (
 
 CREATE TABLE news (
     article_id INT AUTO_INCREMENT,
-    branch_id INT,
-    title VARCHAR(64),
-    content VARCHAR(1024),
+    branch_id INT NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    content VARCHAR(4096) NOT NULL,
     is_public BOOLEAN DEFAULT FALSE,
     date_published DATE DEFAULT (CURRENT_DATE),
     image_url VARCHAR(64),
@@ -59,7 +60,7 @@ CREATE TABLE news (
 CREATE TABLE user_branch_affiliation (
     branch_id INT,
     user_id BINARY(16),
-    is_manager BOOLEAN NOT NULL DEFAULT FALSE,
+    is_manager BOOLEAN DEFAULT FALSE,
 
     PRIMARY KEY (branch_id, user_id),
     FOREIGN KEY (branch_id) REFERENCES branches(branch_id),
