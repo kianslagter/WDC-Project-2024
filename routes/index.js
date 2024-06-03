b11ecbf197c2ce6e3e45643fe7ef43c76ed0fc68
Arnav Vaid <arnav.mindcraft@gmail.com>
2024-06-03 12:12:47 +0000

    initial commit - added create news

--- routes/index.js
+++ routes/index.js
@@ -338,6 +338,10 @@ router.get('/branches', function (req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'public', 'branches.html'));
 });
 
+router.get('/private_policy', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'private_policy.html'));
+});
+
 router.get('/admin/branches/create', function(req, res, next) {
   res.sendFile(path.join(__dirname, '..', 'public', 'create_branch.html'));
 });
