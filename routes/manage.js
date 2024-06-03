1c248306005e39477f534da2f03329d121668db7
Kian Slagter <kianslagter@gmail.com>
2024-06-03 06:55:51 +0000

    view rsvp responses done

--- routes/manage.js
+++ routes/manage.js
@@ -1,53 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/event/responses/:eventID', function(req, res, next){
-    // Should check manager is manager of the branch the event is owned by
+router.post('/event/responses/:eventID', function (req, res, next) {
+  // Should check manager is manager of the branch the event is owned by
 
-    // Get the yeses
-    var yes_responses;
-    req.pool.getConnection( function(err,connection) {
+  // Get the yeses
+  var yes_responses;
+  req.pool.getConnection(function (err, connection) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    const yesQuery = `
+    SELECT CONCAT(users.first_name, ' ', users.last_name) AS name, users.username AS id
+    FROM user_event_attendance AS attendance
+    INNER JOIN users ON users.username = attendance.user_id
+    WHERE attendance.event_id = ? AND attendance.RSVP = TRUE;
+`;
+    connection.query(yesQuery, [req.params.eventID], function (err, yesRows) {
+      connection.release(); // release connection
+      if (err) {
+        console.log(err);
+        res.sendStatus(500);
+        return;
+      }
+      const yesResponses = structuredClone(yesRows);
+
+      // Get the nos
+      var no_responses;
+      req.pool.getConnection(function (err, connection) {
         if (err) {
           console.log(err);
           res.sendStatus(500);
           return;
         }
-        var query = "SELECT CONCAT(users.first_name, ' ' ,users.last_name) as name, users.username as id FROM user_event_attendance AS attendance INNER JOIN users on users.username = attendance.username WHERE attendance.event_id=? AND attendance.RSVP=TRUE;";
-        connection.query(query, [req.params.eventID], function(err, rows, fields) {
+        const noQuery = `
+        SELECT CONCAT(users.first_name, ' ', users.last_name) AS name, users.username AS id
+        FROM user_event_attendance AS attendance
+        INNER JOIN users ON users.username = attendance.user_id
+        WHERE attendance.event_id = ? AND attendance.RSVP = FALSE;
+    `;
+        connection.query(noQuery, [req.params.eventID], function (err, noRows) {
           connection.release(); // release connection
           if (err) {
             console.log(err);
             res.sendStatus(500);
             return;
           }
-          yes_responses = structuredClone(rows);
-           // Get the nos
-            var no_responses;
-            req.pool.getConnection( function(err,connection) {
-                if (err) {
-                console.log(err);
-                res.sendStatus(500);
-                return;
-                }
-                var query = "SELECT CONCAT(users.first_name, ' ' ,users.last_name) as name, users.username as id FROM user_event_attendance AS attendance INNER JOIN users on users.username = attendance.username WHERE attendance.event_id=? AND attendance.RSVP=FALSE;";
-                connection.query(query, [req.params.eventID], function(err, rows, fields) {
-                connection.release(); // release connection
-                if (err) {
-                    console.log(err);
-                    res.sendStatus(500);
-                    return;
-                }
-                no_responses = structuredClone(rows);
+          const noResponses = structuredClone(noRows);
 
-                let resp_body = {"yes": yes_responses, "no": no_responses};
-                res.type('json');
-                res.send(JSON.stringify(resp_body));
-                return;
-                });
-            });
+          const resp_body = {
+            yes: yesResponses,
+            no: noResponses
+          };
+          res.type('json');
+          res.send(JSON.stringify(resp_body));
           return;
         });
+      });
+      return;
     });
+  });
 });
 
 router.post('/event/create', function(req, res, next){
