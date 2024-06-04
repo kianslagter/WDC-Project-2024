c8c14ca459e954ab88ed1d09663a86e7915dda83
Arnav Vaid <arnav.mindcraft@gmail.com>
2024-06-04 11:36:58 +0000

    added view members and view rsvp

--- public/App.js
+++ public/App.js
@@ -95,6 +95,37 @@ const people = [
     },
 ];
 
+const people = [
+    {
+        id: 1,
+        name: 'John Doe',
+        address: '129 Waymouth Street, Adelaide SA 5000',
+        phone: '0412345678',
+        email: 'johndoe@mealmates.com'
+    },
+    {
+        id: 2,
+        name: 'Mike Smith',
+        address: '129 Waymouth Street, Adelaide SA 5000',
+        phone: '0412345678',
+        email: 'mikesmith@mealmates.com'
+    },
+    {
+        id: 3,
+        name: 'Earl Grey',
+        address: '129 Waymouth Street, Adelaide SA 5000',
+        phone: '0412345678',
+        email: 'earl@mealmates.com'
+    },
+    {
+        id: 4,
+        name: 'Adam James',
+        address: '129 Waymouth Street, Adelaide SA 5000',
+        phone: '0412345678',
+        email: 'adamjames@mealmates.com'
+    },
+];
+
 const testUpdateDetails = {
     id: 3,
     title: 'Welcome Adelaide!',
